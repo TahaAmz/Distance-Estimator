4a8ba99fddaea2bf8628a87bad98a8393fed2d9c
Taha AMZ <tahaahmadzadeh76@gmail.com>
2024-04-12 17:45:36 +0330

    Distance

--- Distance.py
+++ Distance.py
@@ -1,38 +1,45 @@
 import cv2
-import requests
-# from ultralytics import YOLO
-# from ultralytics.models.yolo.detect.predict import DetectionPredictor
+import torch
 
 # Distance constants
-KNOWN_DISTANCE = 40.0 #Cm
-MOBILE_WIDTH = 8.0 #Cm
+KNOWN_DISTANCE = 40.0  #Cm
+MOBILE_WIDTH = 8.0  #Cm
 
 # Object detector constant
 CONFIDENCE_THRESHOLD = 0.4
 NMS_THRESHOLD = 0.3
 
 # colors for object detection and texts
-COLORS = [(255,0,0),(255,0,255),(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
-RED = (0,0,255)
-BLACK =(0,0,0)
+COLORS = [(255, 0, 0), (255, 0, 255), (0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
+RED = (0, 0, 255)
+BLACK = (0, 0, 0)
 FONT = cv2.FONT_HERSHEY_COMPLEX
 
-# getting class names from coco.txt and setting up YOLOv5
+# getting class names from coco.txt
 with open("coco.txt", "r") as f:
     class_names = [cName.strip() for cName in f.readlines()]
 
+# connecting to a camera
+# url_cap = "http://192.168.62.215:8080"
+# frame_cap = cv2.VideoCapture(url_cap + "/video")
+frame_cap = cv2.VideoCapture(0)
+
 # setting up opencv net with YOLOv4
-yoloNet = cv2.dnn.readNet('yolov4-tiny.weights', 'yolov4-tiny.cfg')
+yoloNet = cv2.dnn.readNet("yolov4-tiny.cfg", "yolov4-tiny.weights")
 yoloNet.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
 yoloNet.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
 model = cv2.dnn_DetectionModel(yoloNet)
-model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
+model.setInputParams(size=(416, 416), scale=1 / 255, swapRB=True)
+
+
+# setting up opencv net with YOLOv5
+
 
 # object detector function
 def object_detector(image):
-    classes, scores, boxes = model.detect(image, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
+    classes, confidences, boxes = model.detect(image, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
     data_list = []
-    for (classid, score, box) in zip(classes, scores, boxes):
+    for (classid, confidence, box) in zip(classes, confidences, boxes):
         # define color of each, object based on its class id
         color = COLORS[int(classid) % len(COLORS)]
         label = f"{class_names[classid]}"
@@ -43,31 +50,29 @@ def object_detector(image):
 
         # adding class id
         # 1: class name  2: object width in pixels  3: position where to draw distance
-        if classid == 67: # phone class id
+        if classid == 67:  # phone class id
             data_list.append([class_names[classid], box[2], (box[0], box[1] - 2)])
 
     return data_list
 
+
 # focal length finder function
-def focal_length_finder (measured_distance, real_width, width_in_rf):
+def focal_length_finder(measured_distance, real_width, width_in_rf):
     focal_length = (width_in_rf * measured_distance) / real_width
     return focal_length
 
+
 # distance finder function
-def distance_finder(focal_length, real_object_width, width_in_frmae):
-    distance = (real_object_width * focal_length) / width_in_frmae
+def distance_finder(focal_length, real_object_width, width_in_frame):
+    distance = (real_object_width * focal_length) / width_in_frame
     return distance
 
-# reading the reference image and finging focal length
+
+# reading the reference image and finding focal length
 mobile_ref = cv2.imread("RefImages\image3.png")
 mobile_data = object_detector(mobile_ref)
 mobile_width_in_pxl = mobile_data[0][1]
 focal_mobile = focal_length_finder(KNOWN_DISTANCE, MOBILE_WIDTH, mobile_width_in_pxl)
-
-# connecting to camera and begin the detection
-# url_cap = "http://192.168.62.215:8080"
-# frame_cap = cv2.VideoCapture(url_cap + "/video")
-frame_cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = frame_cap.read()
@@ -76,15 +81,14 @@ while True:
         if d[0] == 'cell phone':
             distance = distance_finder(focal_mobile, MOBILE_WIDTH, d[1])
             x, y = d[2]
-
-        cv2.rectangle(frame, (x, y - 3), (x + 150, y + 23), BLACK, -1)
-        cv2.putText(frame, f'Dis: {round(distance, 2)} Cm', (x + 5, y + 13),
-                     FONT, 0.38, RED, 1)
+        distance_txt = round(distance, 2)
+        cv2.rectangle(frame, (x, y), (x + 100, y + 20), BLACK, -1)
+        cv2.putText(frame, f"Dis: {distance_txt} Cm", (x + 5, y + 13), FONT, 0.35, RED, 1)
 
     cv2.imshow("frame", frame)
 
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(10) == 27:
         break
-        
+
 cv2.destroyAllWindows()
 frame_cap.release()
